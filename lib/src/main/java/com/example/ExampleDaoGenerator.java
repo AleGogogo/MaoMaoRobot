bed098fb87f7262052a328edf2d98748f516083c
liuyuwei615 <liuyuwei61566@126.com>
2016-08-30 17:32:47 +0800

    greenDAO

---
+++ lib/src/main/java/com/example/ExampleDaoGenerator.java
@@ -0,0 +1,26 @@
+package com.example;
+
+
+import de.greenrobot.daogenerator.DaoGenerator;
+import de.greenrobot.daogenerator.Entity;
+import de.greenrobot.daogenerator.Schema;
+
+public class ExampleDaoGenerator {
+    public static final String DAO_PATH = "C:/Users/LYW/AndroidStudioProjects/M" +
+            "aoMaoRobot/app/src/main/java-gen";
+    public static final String PACKAGE_NAME = "com.lyw.maomaorobot.greendao";
+
+    public static void main(String[] args)throws Exception{
+        Schema schema = new Schema(1,PACKAGE_NAME);
+        addNote(schema);
+        new DaoGenerator().generateAll(schema,DAO_PATH);
+    }
+
+    private static void addNote(Schema schema) {
+        Entity note = schema.addEntity("Note");
+        note.addIdProperty().primaryKey().autoincrement();
+        note.addIntProperty("plus");
+        note.addStringProperty("tips");
+    }
+
+}
